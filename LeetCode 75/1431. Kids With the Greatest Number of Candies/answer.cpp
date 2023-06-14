50735a334b021d1d2ab752b95f5df09f703abc97
Venkata Sai Reddy <avulurivenkatasaireddy@gmail.com>
2023-06-14 20:22:46 +0530

    1431. Kids With the Greatest Number of Candies

---
+++ LeetCode 75/1431. Kids With the Greatest Number of Candies/answer.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+    {
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        vector<bool> res;
+        for (int i = 0; i < candies.size(); ++i)
+        {
+            if (candies[i] + extraCandies >= maxCandies)
+                res.push_back(true);
+            else
+            {
+                res.push_back(false);
+            }
+        }
+        return res;
+    }
+};
